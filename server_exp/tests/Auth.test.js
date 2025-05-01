c925611726e5ed7d367a69a813943f57a1078f22
Dmitry Dziuba <d. y. dziuba@student. khai. edu>
2025-05-01 15:47:08 +0300

    add auth tests

---
+++ server_exp/tests/Auth.test.js
@@ -0,0 +1,35 @@
+const userRepository = require('../repositories/UserRepository');
+const crypto = require('crypto');
+
+describe('LoginRoute', () => {
+    const hash = crypto.createHash('md5')
+        .update((new Date()).toISOString())
+        .digest('hex');
+    const email = `${hash}@test.fastshare.localhost`
+    const password = email + '_{dJsMN2}'
+
+    async function getTestUser() {
+        let user = await userRepository.getUserByEmail(email);
+        if(user) {
+            return user;
+        }
+        return await userRepository.createUser(hash, email, password);
+    }
+
+    test('Check creating a new user', async () => {
+        const user = await getTestUser();
+        expect(!!user).toEqual(true);
+    });
+
+    test('Check login wrong credentials', async () => {
+        const user = await getTestUser();
+        let passed = user && !await userRepository.verifyPassword(user, 'wrong password');
+        expect(passed).toEqual(true);
+    });
+
+    test('Check login correct credentials', async () => {
+        const user = await getTestUser();
+        let passed = user && await userRepository.verifyPassword(user, password);
+        expect(passed).toEqual(true);
+    });
+});
